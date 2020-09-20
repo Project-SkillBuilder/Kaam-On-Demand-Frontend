589f145f3217f57126a029e240fd242276f695a1
Shantanu-2318 <shansri2318@gmail.com>
2020-09-21 03:14:50 +0530

    Basic Starter Files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1 className="Tagline">Get Your Work Done</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
