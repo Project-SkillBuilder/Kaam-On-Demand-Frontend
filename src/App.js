93270d188e5289cf5ee88dc6b02f8487754bfa09
Shantanu-2318 <shansri2318@gmail.com>
2020-12-05 00:13:53 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SignInUp from "./Components/Sign-in Form/Sign-in";
 import './App.css';
 import NavBar from "./Components/Navigation Bar/Navigation Bar";
 import {
