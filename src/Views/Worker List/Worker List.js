1b4f9f1e96606332cb14d1f34ddebeb143d9be32
Shantanu-2318 <shansri2318@gmail.com>
2021-01-28 19:35:19 +0530

    minor changes

---
+++ src/Views/Worker List/Worker List.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './workerlist.css';
+import WorkerCard from "../../Components/Worker Card/Worker Card";
+import SFpane from "../../Components/Sort-Filter Pane/Sort-Filter Pane";
+import SearchBar from "../../Components/Search Bar/Search Bar";
+
+const Workerlist = () => {
+    return (
+        <div className="Worker_list_main">
+            <div className="Worker_list_sbar text-center">
+                <SearchBar/>
+            </div>
+            <div className="Worker_list_div">
+                <div className="Worker_list_pane">
+                    <SFpane/>
+                </div>
+                <div className="Worker_list_container">
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Workerlist;
