92541b738e2f41ae96db08b389747ceae4823fc5
SHREY ASTHANA <shreyasthana545@gmail.com>
2021-02-01 04:55:38 +0530

    changes in landing page, category page and workers list.

--- src/Views/Category Page/Category Page.js
+++ src/Views/Category Page/Category Page.js
@@ -2,31 +2,36 @@ import React from "react";
 import './Category Page.css';
 import CCard from "../../Components/Category Card/Category Card";
 import SearchBar from "../../Components/Search Bar/Search Bar";
+import kod from "./logo.jpg";
 
 const CatPage = () => {
     return (
-                <div className="Category_Page_main container">
-                    <div className="Category_Page_Sbar text-center">
-                        <SearchBar/>
-                    </div>
-                    <div className="Category_Page_container">
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
-                        <CCard/>
+                <div className="main-body">
+                    <img id="bg" src={kod} alt="background" />
+                    <div className="Category_Page_main container">
+                        <div className="Category_Page_Sbar text-center">
+                            <SearchBar/>
+                        </div>
+                        <div className="Category_Page_container">
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                            <CCard/>
+                        </div>
                     </div>
                 </div>
+
     );
 }
 export default CatPage;
--- src/Views/Worker List/Worker List.js
+++ src/Views/Worker List/Worker List.js
@@ -3,24 +3,28 @@ import './workerlist.css';
 import WorkerCard from "../../Components/Worker Card/Worker Card";
 import SFpane from "../../Components/Sort-Filter Pane/Sort-Filter Pane";
 import SearchBar from "../../Components/Search Bar/Search Bar";
+import kod from "../Category Page/logo.jpg";
 
 const Workerlist = () => {
     return (
         <div className="Worker_list_main">
-            <div className="Worker_list_sbar">
-                <SearchBar/>
-            </div>
-            <div className="Worker_list_container">
-                <WorkerCard/>
-                <WorkerCard/>
-                <WorkerCard/>
-                <WorkerCard/>
-                <WorkerCard/>
-                <WorkerCard/>
-                <WorkerCard/>
-            </div>
-            <div className="Worker_list_pane">
-                <SFpane/>
+            <img id="bg" src={kod} alt="background" />
+            <div className="main-body">
+                <div className="Worker_list_sbar">
+                    <SearchBar/>
+                </div>
+                <div className="Worker_list_container">
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                    <WorkerCard/>
+                </div>
+                <div className="Worker_list_pane">
+                    <SFpane/>
+                </div>
             </div>
         </div>
     );
