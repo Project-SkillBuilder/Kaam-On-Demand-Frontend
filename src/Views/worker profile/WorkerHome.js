5d89443396093a26a98096a9e0bfaa9093293456
Shrey Asthana <shrey.asthana@leverageedu.com>
2023-04-13 13:43:44 +0530

    workerhome added

--- src/Views/worker profile/WorkerHome.js
+++ src/Views/worker profile/WorkerHome.js
@@ -1,121 +1,174 @@
 import React from "react";
-import './style.css';
+import "./style.css";
 import logo from "./logo.jpg";
 
 function WProfile() {
-    
-    return(
-        <div className="container">
-            <div className="profile-header">
-                <div className="profile-img">
-                    <img src={logo} width="200" alt=""/>
-                </div>
-                <div className="profile-nav-info">
-                    <h3 className="user-name">KOD</h3>
-                    <div className="address">
-                        <p className="state">Uttar Pradesh, </p>
-                        <span className="country">India.</span>
-                    </div>
-                </div>
-                <div className="profile-option">
-                    <div className="notification">
-                        <i className="fa fa-bell"></i>
-                        <span className="alert-message">1</span>
-                    </div>
-                </div>
-            </div>
-            <div className="main-bd">
-                <div className="left-side">
-                    <div className="profile-side">
-                        <p className="mobile-no"><i className="fa fa-phone"></i>+91{}</p>
-                        <p className="user-mail"><i className="fa fa-envelope"></i>xxx@gmail.com</p>
-                        <div className="user-bio">
-                            <h3>Bio</h3>
-                            <p className="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit
-                                atque dolor quidem, ullam, voluptate molestias! Lorem ipsum dolor sit amet.</p>
-                        </div>
-                        <div className="profile-btn">
-                            {/*<button className="mkcbtn">*/}
-                            {/*    <i className="fa fa-comment"></i> mkc*/}
-                            {/*</button>*/}
-                            <button className="editbtn">
-                                <i className="fa fa-plus"></i>Edit
-                            </button>
-                        </div>
-                        <div className="user-rating">
-                            <h3 className="rating">4.5</h3>
-                            <div className="rate">
-                                <div className="stars">
-
-                                    <i className="fa fa-star"></i>
-                                    <i className="fa fa-star"></i>
-                                    <i className="fa fa-star"></i>
-                                    <i className="fa fa-star"></i>
-                                    <i className="fa fa-star"></i>
-                                </div>
-                                <span className="no-user"><span>123</span>&nbsp;&nbsp; reviews</span>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div className="right-side">
-                    <ul className="nav mb-3" id="tabs-tab" role="tablist">
-                        <li className="nav-item user-post" role="presentation">
-                            <a className="nav-link active" id="tabs-home-tab" data-toggle="tab" href="#tabs-home"
-                               role="tab" aria-controls="tabs-home" aria-selected="true">Home</a>
-                        </li>
-                        <li className="nav-item user-review" role="presentation">
-                            <a className="nav-link" id="tabs-profile-tab" data-toggle="tab" href="#tabs-profile"
-                               role="tab" aria-controls="tabs-profile" aria-selected="false">Profile</a>
-                        </li>
-                        <li className="nav-item user-setting" role="presentation">
-                            <a className="nav-link" id="tabs-contact-tab" data-toggle="tab" href="#tabs-contact"
-                               role="tab" aria-controls="tabs-contact" aria-selected="false">Contact</a>
-                        </li>
-                    </ul>
-                    <div className="tab-content profile-body" id="tabs-tabContent">
-                        <div className="profile-posts tab-pane fade show active" id="tabs-home" role="tabpanel"
-                             aria-labelledby="tabs-home-tab">
-                            <h1>Posts</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores iure quae sint magni
-                                cupiditate non, quod velit similique ducimus! Impedit dicta, sit officiis rerum.
-                                Adipisci ut facere atque nostrum odit dolorum ea cumque modi. Asperiores dolorum, iure
-                                odit ad, tempore magnam beatae deleniti soluta quisquam assumenda pariatur incidunt
-                                sapiente eaque modi earum. Ab ducimus accusamus recusandae, odio iusto commodi fuga,
-                                repellat inventore iure aut libero consequatur sequi nesciunt dolorem ipsam.
-                            </p>
-                        </div>
-                        <div className="profile-reviews tab-pane fade" id="tabs-profile" role="tabpanel"
-                             aria-labelledby="tabs-profile-tab">
-                            <h1>Reviews</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores iure quae sint magni
-                                cupiditate non, quod velit similique ducimus! Impedit dicta, sit officiis rerum.
-                                Adipisci ut facere atque nostrum odit dolorum ea cumque modi. Asperiores dolorum, iure
-                                odit ad, tempore magnam beatae deleniti soluta quisquam assumenda pariatur incidunt
-                                sapiente eaque modi earum. Ab ducimus accusamus recusandae, odio iusto commodi fuga,
-                                repellat inventore iure aut libero consequatur sequi nesciunt dolorem ipsam.
-                            </p>
-                        </div>
-                        <div className="profile-settings tab-pane fade" id="tabs-contact" role="tabpanel"
-                             aria-labelledby="tabs-contact-tab">
-                            <h1>Settings</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores iure quae sint magni
-                                cupiditate non, quod velit similique ducimus! Impedit dicta, sit officiis rerum.
-                                Adipisci ut facere atque nostrum odit dolorum ea cumque modi. Asperiores dolorum, iure
-                                odit ad, tempore magnam beatae deleniti soluta quisquam assumenda pariatur incidunt
-                                sapiente eaque modi earum. Ab ducimus accusamus recusandae, odio iusto commodi fuga,
-                                repellat inventore iure aut libero consequatur sequi nesciunt dolorem ipsam.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="profile-header">
+        <div className="profile-img">
+          <img src={logo} width="200" alt="" />
         </div>
-
-    );
+        <div className="profile-nav-info">
+          <h3 className="user-name">KOD</h3>
+          <div className="address">
+            <p className="state">Uttar Pradesh, </p>
+            <span className="country">India.</span>
+          </div>
+        </div>
+        <div className="profile-option">
+          <div className="notification">
+            <i className="fa fa-bell"></i>
+            <span className="alert-message">1</span>
+          </div>
+        </div>
+      </div>
+      <div className="main-bd">
+        <div className="left-side">
+          <div className="profile-side">
+            <p className="mobile-no">
+              <i className="fa fa-phone"></i>+91{}
+            </p>
+            <p className="user-mail">
+              <i className="fa fa-envelope"></i>xxx@gmail.com
+            </p>
+            <div className="user-bio">
+              <h3>Bio</h3>
+              <p className="bio">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Reprehenderit atque dolor quidem, ullam, voluptate molestias!
+                Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className="profile-btn">
+              {/*<button className="mkcbtn">*/}
+              {/*    <i className="fa fa-comment"></i> mkc*/}
+              {/*</button>*/}
+              <button className="editbtn">
+                <i className="fa fa-plus"></i>Edit
+              </button>
+            </div>
+            <div className="user-rating">
+              <h3 className="rating">4.5</h3>
+              <div className="rate">
+                <div className="stars">
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </div>
+                <span className="no-user">
+                  <span>123</span>&nbsp;&nbsp; reviews
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right-side">
+          <ul className="nav mb-3" id="tabs-tab" role="tablist">
+            <li className="nav-item user-post" role="presentation">
+              <a
+                className="nav-link active"
+                id="tabs-home-tab"
+                data-toggle="tab"
+                href="#tabs-home"
+                role="tab"
+                aria-controls="tabs-home"
+                aria-selected="true"
+              >
+                Home
+              </a>
+            </li>
+            <li className="nav-item user-review" role="presentation">
+              <a
+                className="nav-link"
+                id="tabs-profile-tab"
+                data-toggle="tab"
+                href="#tabs-profile"
+                role="tab"
+                aria-controls="tabs-profile"
+                aria-selected="false"
+              >
+                Profile
+              </a>
+            </li>
+            <li className="nav-item user-setting" role="presentation">
+              <a
+                className="nav-link"
+                id="tabs-contact-tab"
+                data-toggle="tab"
+                href="#tabs-contact"
+                role="tab"
+                aria-controls="tabs-contact"
+                aria-selected="false"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div className="tab-content profile-body" id="tabs-tabContent">
+            <div
+              className="profile-posts tab-pane fade show active"
+              id="tabs-home"
+              role="tabpanel"
+              aria-labelledby="tabs-home-tab"
+            >
+              <h1>Posts</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolores iure quae sint magni cupiditate non, quod velit
+                similique ducimus! Impedit dicta, sit officiis rerum. Adipisci
+                ut facere atque nostrum odit dolorum ea cumque modi. Asperiores
+                dolorum, iure odit ad, tempore magnam beatae deleniti soluta
+                quisquam assumenda pariatur incidunt sapiente eaque modi earum.
+                Ab ducimus accusamus recusandae, odio iusto commodi fuga,
+                repellat inventore iure aut libero consequatur sequi nesciunt
+                dolorem ipsam.
+              </p>
+            </div>
+            <div
+              className="profile-reviews tab-pane fade"
+              id="tabs-profile"
+              role="tabpanel"
+              aria-labelledby="tabs-profile-tab"
+            >
+              <h1>Reviews</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolores iure quae sint magni cupiditate non, quod velit
+                similique ducimus! Impedit dicta, sit officiis rerum. Adipisci
+                ut facere atque nostrum odit dolorum ea cumque modi. Asperiores
+                dolorum, iure odit ad, tempore magnam beatae deleniti soluta
+                quisquam assumenda pariatur incidunt sapiente eaque modi earum.
+                Ab ducimus accusamus recusandae, odio iusto commodi fuga,
+                repellat inventore iure aut libero consequatur sequi nesciunt
+                dolorem ipsam.
+              </p>
+            </div>
+            <div
+              className="profile-settings tab-pane fade"
+              id="tabs-contact"
+              role="tabpanel"
+              aria-labelledby="tabs-contact-tab"
+            >
+              <h1>Settings</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Dolores iure quae sint magni cupiditate non, quod velit
+                similique ducimus! Impedit dicta, sit officiis rerum. Adipisci
+                ut facere atque nostrum odit dolorum ea cumque modi. Asperiores
+                dolorum, iure odit ad, tempore magnam beatae deleniti soluta
+                quisquam assumenda pariatur incidunt sapiente eaque modi earum.
+                Ab ducimus accusamus recusandae, odio iusto commodi fuga,
+                repellat inventore iure aut libero consequatur sequi nesciunt
+                dolorem ipsam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-    export default WProfile;
+export default WProfile;
---
+++ src/Views/worker profile/WorkerNav.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Navigation Bar.css";
+import kod from "./kod.png";
+import CustomerProfile from "../../Views/Customer Profile/Customer Profile";
+import CatPage from "../../Views/Category Page/Category Page";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import LandingPage from "../../Views/Landing Page/Landing Page";
+import Workerlist from "../../Views/Worker List/Worker List";
+import Workerinfo from "../../Views/Worker Info/Worker Info";
+
+
+const WorkerNav = () => {
+    return (
+            <div className="Navigation_Bar_body Navigation_Bar_*">
+                <nav className="navbar navbar-expand-sm navbar-light position-sticky z-9999 top-0">
+                    <a className="Navigation_Bar_a navbar-brand" href="#"><img className="logo" src={kod} alt="logo"/></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-end" id="navbarNavDropdown">
+                        <ul className="navbar-nav nav_links">
+                            <li className="nav-item active">
+                                <Link to="/WorkerHome">
+                                    <a className="Navigation_Bar_a nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/WorkerHome/WorkerProfile">
+                                    <a className="Navigation_Bar_a nav-link" href="#">My Profile</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/">
+                                    <a className="Navigation_Bar_a nav-link" href="#">Sign-Out</a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <div>
+                    <Switch>
+                        <Route path="/Home/WorkerList/WorkerInfo/:number">
+                            <Workerinfo/>
+                        </Route>
+                        <Route path="/Home/WorkerList/:Category">
+                            <Workerlist/>
+                        </Route>
+                        <Route path="/Home/CustomerProfile/">
+                            <CustomerProfile/>
+                        </Route>
+                        <Route path="/Home">
+                            <CatPage/>
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+    );
+}
+
+export default WorkerNav;
